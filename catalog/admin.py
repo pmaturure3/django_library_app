a0b9f70a28237aaf3baee334b746b195c9ab7df6
dfili8 <dfilimon.s@gmail.com>
2019-11-10 13:58:25 -0500

    First version of library app

---
+++ catalog/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from catalog.models import Author, Book, Genre, Language, BookInstance
+
+
+# Define the admin class
+class BookInlineTab(admin.StackedInline):
+    model = Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInlineTab]
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'borrower', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        ('Book Info', {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back', 'borrower')
+        }),
+    )
+
+
+# Register the admin class with the associated model
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+
